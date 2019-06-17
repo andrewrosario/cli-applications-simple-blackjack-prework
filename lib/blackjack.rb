b2cf75ccef9fc39b207718d325e1969ffa051821
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-17 02:40:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,5 +61,13 @@ end
 
 def runner
   # code runner here
+  card_total = 0
+  welcome
+  card_total = initial_round
+  display_card_total(card_total)
+  prompt_user
+  hit?(card_total)
+  
+  
 end
     
