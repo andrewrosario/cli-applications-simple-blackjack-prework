1b299a473a395912b35e68764ad126cd3efaf4ed
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-17 02:42:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,7 +64,11 @@ def runner
   card_total = 0
   welcome
   card_total = initial_round
-  hit?(card_total)
+  until card_total > 21
+    card_total += hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game
   
   
 end
